9a05cf2eeff448bafb830963038bf4be0bba976f
Jayreny <ja99.je@gmail.com>
2017-02-13 15:18:56 +0000

    YO

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Write your function calls below this line
+setColor(4, 3, "purple");
+setColor(4, 6, "purple");
+setColor(6, 2, "purple");
+setColor(7, 3, "purple");
+setColor(7, 3, "purple");
+setColor(8, 4, "purple");
+setColor(8, 5, "purple");
+setColor(7, 6, "purple");
+setColor(6, 7, "purple");
